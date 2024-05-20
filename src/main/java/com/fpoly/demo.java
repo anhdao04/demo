53a187dc13ba51a5ff6f85b43a5e4e2a402ce103
dao anh <anhdaovu070430@gmail.com>
2024-05-20 15:37:29 +0700

    Demo

---
+++ src/main/java/com/fpoly/demo.java
@@ -0,0 +1,13 @@
+package com.fpoly;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Controller
+public class demo {
+
+}
